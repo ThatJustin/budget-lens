b73901e5826e3d79e37a3f726ffed5d44a266dc4
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2022-09-22 20:47:27 -0400

    test trial

--- app/src/androidTest/java/com/codenode/budgetlens/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/codenode/budgetlens/ExampleInstrumentedTest.kt
@@ -18,7 +18,6 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.codenode.budgetlens", appContext.packageName)
     }
