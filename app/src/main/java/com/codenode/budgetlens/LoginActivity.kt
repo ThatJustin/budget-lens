b00e25540feb96cbfb02408fac9821c65b2c8668
Massimo Lopez <mlopez9876@gmail.com>
2022-10-07 10:48:46 -0400

    feat: Rough xml file for signup page. Clicking register button in login page goes to signup page

--- app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.codenode.budgetlens
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.codenode.budgetlens.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        binding = ActivityLoginBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_login)
 
 
@@ -14,5 +22,17 @@ class LoginActivity : AppCompatActivity() {
         actionBar!!.title = "Login"
 
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        val registerButton: Button = findViewById(R.id.createNewUser)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+//        binding.createNewUser.setOnClickListener {
+//            val intent = Intent(this, SignUpActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
