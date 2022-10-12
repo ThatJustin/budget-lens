d0903f0d74bd07b9a1b266210804738af90687ec
Mateo <mateo.palomino21@hotmail.com>
2022-10-11 21:43:06 -0400

    feat: BUD-56 using okhttp

--- app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
@@ -2,6 +2,11 @@ package com.codenode.budgetlens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +19,46 @@ class LoginActivity : AppCompatActivity() {
         actionBar!!.title = "Login"
 
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        val loginButton: Button = findViewById(R.id.checkCredentials)
+
+        loginButton.setOnClickListener {
+            val url = "http://192.168.2.56:8000/registerEndpoint/"
+
+            val registrationPost = OkHttpClient()
+
+            val mediaType = "application/json".toMediaTypeOrNull()
+
+            val body = ("{\r\n" +
+                    "    \"username\": \"teo14\",\r\n" +
+                    "    \"password\": \"1234palo\"\r\n" +
+                    "}").trimIndent().toRequestBody(mediaType)
+
+            val request = Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build()
+
+            registrationPost.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: java.io.IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.i("Response", "Got the response from server")
+                    response.use {
+                        if (response.isSuccessful) {
+                            Log.i("Successful", "${response.body?.string()}")
+                        } else {
+                            Log.e(
+                                "Error",
+                                "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
+                            )
+                        }
+                    }
+                }
+            })
+        }
     }
 }
