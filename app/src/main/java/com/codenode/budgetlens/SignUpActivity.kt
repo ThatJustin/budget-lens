e65827df7b4a0eb9ad4199195a4dbc0afa8463b9
Mateo <mateo.palomino21@hotmail.com>
2022-10-12 15:05:35 -0400

    feat: BUD-56 adding sign up to this branch to help with okhttp request

--- app/src/main/java/com/codenode/budgetlens/SignUpActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/SignUpActivity.kt
@@ -1,45 +1,45 @@
 package com.codenode.budgetlens
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
+import okio.IOException
 
-class LoginActivity : AppCompatActivity() {
+
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
+        setContentView(R.layout.activity_sign_up)
 
         val actionBar = supportActionBar
 
-        actionBar!!.title = "Login"
+        actionBar!!.title = "Sign Up"
 
         actionBar.setDisplayHomeAsUpEnabled(true)
 
-        val registerButton: Button = findViewById(R.id.createNewUser)
+        val registerButton: Button = findViewById(R.id.filledButton)
 
         registerButton.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        val loginButton: Button = findViewById(R.id.checkCredentials)
-
-        loginButton.setOnClickListener {
-            val url = "https://${BuildConfig.IP_ADDRESS}:${BuildConfig.PORT_NUMBER}/loginEndpoint/"
+            val url = "http://${BuildConfig.IP_ADDRESS}:${BuildConfig.PORT_NUMBER}/registerEndpoint/"
 
             val registrationPost = OkHttpClient()
 
             val mediaType = "application/json".toMediaTypeOrNull()
 
             val body = ("{\r\n" +
-                    "    \"username\": \"teo14\",\r\n" +
-                    "    \"password\": \"1234palo\"\r\n" +
+                    "    \"user\": {\r\n" +
+                    "        \"username\": \"${findViewById<TextView>(R.id.email).text}\",\r\n" +
+                    "        \"first_name\": \"${findViewById<TextView>(R.id.firstName).text}\",\r\n" +
+                    "        \"last_name\": \"${findViewById<TextView>(R.id.lastName).text}\",\r\n" +
+                    "        \"email\": \"${findViewById<TextView>(R.id.email).text}\",\r\n" +
+                    "        \"password\": \"${findViewById<TextView>(R.id.password).text}\"\r\n" +
+                    "    },\r\n" +
+                    "    \"telephone_number\": 5143572412\r\n" +
                     "}").trimIndent().toRequestBody(mediaType)
 
             val request = Request.Builder()
@@ -56,15 +56,16 @@ class LoginActivity : AppCompatActivity() {
                 override fun onResponse(call: Call, response: Response) {
                     Log.i("Response", "Got the response from server")
                     response.use {
-                        if (response.isSuccessful) {
-                            Log.i("Successful", "${response.body?.string()}")
-                        } else {
+                        if (!response.isSuccessful) {
                             Log.e(
                                 "Error",
                                 "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
                             )
+                        } else {
+                            Log.i("Successful", "${response.body?.string()}")
                         }
                     }
+
                 }
             })
         }
