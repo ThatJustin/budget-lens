fd640d218c46f5c74e0db7d8dbf42def807f15a0
Massimo Lopez <mlopez9876@gmail.com>
2022-10-13 23:26:30 -0400

    feat: Changed redirect after signin go to homepage properly, adding more frontend checks.

--- app/src/main/java/com/codenode/budgetlens/SignUpActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/SignUpActivity.kt
@@ -17,6 +17,7 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var emailField: TextInputEditText
     private lateinit var firstNameField: TextInputEditText
     private lateinit var lastNameField: TextInputEditText
+    private lateinit var telephoneField: TextInputEditText
     private lateinit var confirmPasswordField: TextInputEditText
     private lateinit var passwordField: TextInputEditText
 
@@ -24,11 +25,13 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var registerButton: Button
 
     //    Check if fields have been met
-    private var areFieldsCorrect: Boolean = true
+    private var hasCorrectFields: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val goToWelcomePage = Intent(this, WelcomeActivity::class.java)
 
         val actionBar = supportActionBar
 
@@ -40,6 +43,7 @@ class SignUpActivity : AppCompatActivity() {
         emailField = findViewById(R.id.email)
         firstNameField = findViewById(R.id.firstName)
         lastNameField = findViewById(R.id.lastName)
+        telephoneField = findViewById(R.id.telephoneNumber)
         confirmPasswordField = findViewById(R.id.confirmPassword)
         passwordField = findViewById(R.id.password)
 
@@ -51,12 +55,52 @@ class SignUpActivity : AppCompatActivity() {
             checkFrontendFields()
 
             // Register using the rest API once all the frontend field checks passed
-            if (areFieldsCorrect) {
-                onRegisterRequest()
+            if (hasCorrectFields) {
+                val url = "http://${BuildConfig.IP_ADDRESS}:${BuildConfig.PORT_NUMBER}/registerEndpoint/"
 
-//                After registering, go to the welcome page
-                val intent = Intent(this, WelcomeActivity::class.java)
-                startActivity(intent)
+                val registrationPost = OkHttpClient()
+
+                val mediaType = "application/json".toMediaTypeOrNull()
+
+                val body = ("{\r\n" +
+                        "    \"user\": {\r\n" +
+                        "        \"username\": \"${findViewById<TextView>(R.id.email).text}\",\r\n" +
+                        "        \"first_name\": \"${findViewById<TextView>(R.id.firstName).text}\",\r\n" +
+                        "        \"last_name\": \"${findViewById<TextView>(R.id.lastName).text}\",\r\n" +
+                        "        \"email\": \"${findViewById<TextView>(R.id.email).text}\",\r\n" +
+                        "        \"password\": \"${findViewById<TextView>(R.id.password).text}\"\r\n" +
+                        "    },\r\n" +
+                        "    \"telephone_number\": \"${findViewById<TextView>(R.id.telephoneNumber).text}\"\r\n" +
+                        "}").trimIndent().toRequestBody(mediaType)
+
+                val request = Request.Builder()
+                    .url(url)
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/json")
+                    .build()
+
+                registrationPost.newCall(request).enqueue(object : Callback {
+
+                    override fun onFailure(call: Call, e: java.io.IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        Log.i("Response", "Got the response from server")
+                        response.use {
+                            if (!response.isSuccessful) {
+                                Log.e(
+                                    "Error",
+                                    "Something went wrong${response.body?.string().toString()} ${response.message} ${response.headers}"
+                                )
+                            } else {
+                                Log.i("Successful", "${response.body?.string()}")
+                                startActivity(goToWelcomePage)
+                            }
+                        }
+
+                    }
+                })
             }
         }
     }
@@ -64,27 +108,32 @@ class SignUpActivity : AppCompatActivity() {
     private fun checkFrontendFields() {
         if (emailField.length() == 0) {
             emailField.error = "This field is required"
-            areFieldsCorrect = false
+            hasCorrectFields = false
         }
 
         if (firstNameField.length() == 0) {
             firstNameField.error = "This field is required"
-            areFieldsCorrect = false
+            hasCorrectFields = false
         }
 
         if (lastNameField.length() == 0) {
             lastNameField.error = "This field is required"
-            areFieldsCorrect = false
+            hasCorrectFields = false
+        }
+
+        if (telephoneField.length() == 0) {
+            telephoneField.error = "This field is required"
+            hasCorrectFields = false
         }
 
         if (passwordField.length() == 0) {
             passwordField.error = "This field is required"
-            areFieldsCorrect = false
+            hasCorrectFields = false
         }
 
         if (confirmPasswordField.length() == 0) {
             confirmPasswordField.error = "This field is required"
-            areFieldsCorrect = false
+            hasCorrectFields = false
             return
         }
 
@@ -94,56 +143,16 @@ class SignUpActivity : AppCompatActivity() {
                 "passwords don't match:${confirmPasswordField.text.toString()}!=${passwordField.text.toString()}"
             )
             confirmPasswordField.error = "Passwords do not match"
-            areFieldsCorrect = false
+            hasCorrectFields = false
             return
         }
-        areFieldsCorrect = true
-    }
 
+        if (passwordField.text.toString().length < 8) {
+            passwordField.error = "Password must be greater than 8 character"
+            hasCorrectFields = false
+            return
+        }
 
-    private fun onRegisterRequest() {
-        val url = "http://${BuildConfig.IP_ADDRESS}:${BuildConfig.PORT_NUMBER}/registerEndpoint/"
-
-        val registrationPost = OkHttpClient()
-
-        val mediaType = "application/json".toMediaTypeOrNull()
-
-        val body = ("{\r\n" +
-                "    \"user\": {\r\n" +
-                "        \"username\": \"${findViewById<TextView>(R.id.email).text}\",\r\n" +
-                "        \"first_name\": \"${findViewById<TextView>(R.id.firstName).text}\",\r\n" +
-                "        \"last_name\": \"${findViewById<TextView>(R.id.lastName).text}\",\r\n" +
-                "        \"email\": \"${findViewById<TextView>(R.id.email).text}\",\r\n" +
-                "        \"password\": \"${findViewById<TextView>(R.id.password).text}\"\r\n" +
-                "    },\r\n" +
-                "    \"telephone_number\": 5143572412\r\n" +
-                "}").trimIndent().toRequestBody(mediaType)
-
-        val request = Request.Builder()
-            .url(url)
-            .method("POST", body)
-            .addHeader("Content-Type", "application/json")
-            .build()
-
-        registrationPost.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: java.io.IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                Log.i("Response", "Got the response from server")
-                response.use {
-                    if (!response.isSuccessful) {
-                        Log.e(
-                            "Error",
-                            "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
-                        )
-                    } else {
-                        Log.i("Successful", "${response.body?.string()}")
-                    }
-                }
-
-            }
-        })
+        hasCorrectFields = true
     }
 }
