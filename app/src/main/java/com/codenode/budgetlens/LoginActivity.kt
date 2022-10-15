550c157db92c3623c0b4d55594b1515a816cc447
Mateo <mateo.palomino21@hotmail.com>
2022-10-15 17:31:17 -0400

    feat: BUD-56 Update/Made fixes to login based on requested changes

--- app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -29,10 +30,6 @@ class LoginActivity : AppCompatActivity() {
 
         val actionBar = supportActionBar
 
-        val registerButton: Button = findViewById(R.id.createNewUser)
-
-        var emptyFields = false
-
         usernameField = findViewById<TextInputEditText>(R.id.usernameText)
 
         passwordField = findViewById<TextInputEditText>(R.id.passwordText)
@@ -41,63 +38,61 @@ class LoginActivity : AppCompatActivity() {
 
         actionBar.setDisplayHomeAsUpEnabled(true)
 
-        registerButton.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-
+        // Will eventually need to block login when user is already logged in
         loginButton.setOnClickListener {
-            println(findViewById<TextInputEditText>(R.id.usernameCredential));
-
-            println(findViewById<TextInputEditText>(R.id.passwordCredential));
 
             if(usernameField.length() == 0){
                 usernameField.error = "This field is required"
-                emptyFields = true
             }
             if(passwordField.length() == 0){
                 passwordField.error = "This field is required"
-                emptyFields = true
             }
 
-            val url = "http://IP_ADDRESS_HERE:8000/loginEndpoint/"
+                val url = "http://IP_ADDRESS_HERE:8000/loginEndpoint/"
 
-            val registrationPost = OkHttpClient()
+                val registrationPost = OkHttpClient()
 
-            val mediaType = "application/json".toMediaTypeOrNull()
+                val mediaType = "application/json".toMediaTypeOrNull()
 
-            val body = ("{\r\n" +
-                    "    \"username\": \"${findViewById<TextView>(R.id.usernameText).text}\",\r\n" +
-                    "    \"password\": \"${findViewById<TextView>(R.id.passwordText).text}\"\r\n" +
-                    "}").trimIndent().toRequestBody(mediaType)
+                val body = ("{\r\n" +
+                        "    \"username\": \"${findViewById<TextView>(R.id.usernameText).text}\",\r\n" +
+                        "    \"password\": \"${findViewById<TextView>(R.id.passwordText).text}\"\r\n" +
+                        "}").trimIndent().toRequestBody(mediaType)
 
-            val request = Request.Builder()
-                .url(url)
-                .method("POST", body)
-                .addHeader("Content-Type", "application/json")
-                .build()
+                val request = Request.Builder()
+                    .url(url)
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/json")
+                    .build()
 
-            registrationPost.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    e.printStackTrace()
-                }
+                registrationPost.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
 
-                override fun onResponse(call: Call, response: Response) {
-                    Log.i("Response", "Got the response from server")
-                    response.use {
-                        if (response.isSuccessful) {
-                            Log.i("Successful", "${response.body?.string()}")
-                            startActivity(goToHomePageActivity)
-                        } else {
-                            Log.e(
-                                "Error",
-                                "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
-                            )
+                    override fun onResponse(call: Call, response: Response) {
+                        Log.i("Response", "Got the response from server")
+                        response.use {
+                            if (response.isSuccessful) {
+                                val body = response.body?.string()
+                                if (body != null) {
+                                    Log.i("Successful", body)
+                                    startActivity(goToHomePageActivity)
+                                }
+                                else{
+                                    Log.i("Empty", "Something went wrong${response.body?.string()}")
+                                }
+
+                            } else {
+                                Log.e(
+                                    "Error",
+                                    "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
+                                )
+                            }
                         }
                     }
-                }
-            })
+                })
         }
     }
 }
+class HomePage(val token: String)
