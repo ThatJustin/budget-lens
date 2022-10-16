7dfabbbb1b9efba61d55fa9f28ab3d6ebe46ebd1
shirlywy <49579378+shirlywy@users.noreply.github.com>
2022-10-16 17:02:44 -0400

    BUD-128 login option with google displayed on the login page. & BUD-129 Use OAuth already setup in BUD-55 to Login with google (#8)
    * feat: BUD-56 created the login page
    
    * feat: BUD-56 implemented material design 3 to login page
    
    * feat: BUD-56 updated theme to use proper material3
    
    * Try Google OAuth Google Login
    
    * Rename .java to .kt
    
    * Try Google OAuth Google Login
    
    * Google Login Works but still need modify the xml and LoginActivity.kt
    
    * change Google logo and update the login xml papge
    
    Co-authored-by: Mateo <mateo.palomino21@hotmail.com>

---
+++ app/src/main/java/com/codenode/budgetlens/GoogleLoginActivity.kt
@@ -0,0 +1,68 @@
+package com.codenode.budgetlens
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import android.content.Intent
+import android.widget.ImageView
+import com.google.android.gms.common.api.ApiException
+import android.widget.Toast
+
+class GoogleLoginActivity : AppCompatActivity() {
+    var mGoogleSignInClient: GoogleSignInClient? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account!=null){
+            navigateToSecondActivity()
+        }
+        // Set the dimensions of the sign-in button.
+        val signInButton = findViewById<ImageView>(R.id.google_sign_in)
+        signInButton.setOnClickListener { signIn() }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            //            handleSignInResult(task);
+            try {
+                task.getResult(ApiException::class.java)
+                navigateToSecondActivity()
+            } catch (e: ApiException) {
+                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun navigateToSecondActivity() {
+        finish()
+        val intent = Intent(this@GoogleLoginActivity, GoogleLoginSecondActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 100
+    }
+}
--- app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
@@ -3,6 +3,12 @@ package com.codenode.budgetlens
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -15,7 +21,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
-
+    var mGoogleSignInClient: GoogleSignInClient? = null
     private lateinit var usernameField: TextInputEditText
 
     private lateinit var passwordField: TextInputEditText
@@ -23,7 +29,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         val goToHomePageActivity = Intent(this, HomePageActivity::class.java)
 
         val loginButton: Button = findViewById(R.id.checkCredentials)
@@ -56,50 +61,91 @@ class LoginActivity : AppCompatActivity() {
                 passwordField.error = "This field is required"
             }
 
-                val url = "http://${BuildConfig.ADDRESS}:${BuildConfig.PORT}/loginEndpoint/"
+            val url = "http://${BuildConfig.ADDRESS}:${BuildConfig.PORT}/loginEndpoint/"
 
-                val registrationPost = OkHttpClient()
+            val registrationPost = OkHttpClient()
 
-                val mediaType = "application/json".toMediaTypeOrNull()
+            val mediaType = "application/json".toMediaTypeOrNull()
 
-                val body = ("{\r\n" +
-                        "    \"username\": \"${usernameField.text}\",\r\n" +
-                        "    \"password\": \"${passwordField.text}\"\r\n" +
-                        "}").trimIndent().toRequestBody(mediaType)
+            val body = ("{\r\n" +
+                    "    \"username\": \"${usernameField.text}\",\r\n" +
+                    "    \"password\": \"${passwordField.text}\"\r\n" +
+                    "}").trimIndent().toRequestBody(mediaType)
 
-                val request = Request.Builder()
-                    .url(url)
-                    .method("POST", body)
-                    .addHeader("Content-Type", "application/json")
-                    .build()
+            val request = Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build()
 
-                registrationPost.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                    }
+            registrationPost.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
 
-                    override fun onResponse(call: Call, response: Response) {
-                        Log.i("Response", "Got the response from server")
-                        response.use {
-                            if (response.isSuccessful) {
-                                val body = response.body?.string()
-                                if (body != null) {
-                                    Log.i("Successful", body)
-                                    startActivity(goToHomePageActivity)
-                                }
-                                else{
-                                    Log.i("Empty", "Something went wrong${response.body?.string()}")
-                                }
-
-                            } else {
-                                Log.e(
-                                    "Error",
-                                    "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
-                                )
+                override fun onResponse(call: Call, response: Response) {
+                    Log.i("Response", "Got the response from server")
+                    response.use {
+                        if (response.isSuccessful) {
+                            val body = response.body?.string()
+                            if (body != null) {
+                                Log.i("Successful", body)
+                                startActivity(goToHomePageActivity)
                             }
+                            else{
+                                Log.i("Empty", "Something went wrong${response.body?.string()}")
+                            }
+
+                        } else {
+                            Log.e(
+                                "Error",
+                                "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
+                            )
                         }
                     }
-                })
+                }
+            })
+        }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if(account!=null){
+            navigateToSecondActivity()
+        }
+        val signInButton = findViewById<ImageView>(R.id.google_sign_in)
+        signInButton.setOnClickListener { signIn() }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+         if (requestCode == RC_SIGN_IN) {
+             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                task.getResult(ApiException::class.java)
+                navigateToSecondActivity()
+            } catch (e: ApiException) {
+                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
+
+    private fun navigateToSecondActivity() {
+        finish()
+        val intent = Intent(this@LoginActivity, GoogleLoginSecondActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 100
+    }
+
 }
