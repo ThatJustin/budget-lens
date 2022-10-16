8b656180fcc9787f3265076e860a2911d52ef2fe
Mateo Palomino <60004667+teoPalomino@users.noreply.github.com>
2022-10-15 23:29:24 -0400

    Feat/bud 56 Login Activity
    * feat: BUD-56 created the login page
    
    * feat: BUD-56 implemented material design 3 to login page
    
    * feat: BUD-56 updated theme to use proper material3
    
    * feat: BUD-56 using okhttp
    
    * feat: BUD-56 adding sign up to this branch to help with okhttp request
    
    * feat: BUD-56 Users can now login with username/password
    
    * feat: BUD-56 Temp home page, redirection after login
    
    * feat: BUD-56 Added Empty Field Checks, Updated How Username/Pass Is Obtained.
    
    * feart(navbar): add navbar to the home page
    
    * feat: BUD-56 Remove sign in to ease rebase
    
    * feat: BUD-56 Update/Made fixes to login based on requested changes
    
    * feat: BUD-56 Remove unused xml file
    
    * feat: BUD-56 Removed unnecessary file that implemented code that caused issues
    
    * feat: BUD-56 Redirect to proper homepage
    
    * feat: BUD-56 maybe we will import fonts later, for now I removed it to avoid unnecessary issues
    
    * feat: BUD-56 For some reason my selection during merge was not selected, added signup functionality, added proper url
    
    Co-authored-by: Amir Sadeqi <amirsadeqi@Amirs-MacBook-Air.local>

--- app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/LoginActivity.kt
@@ -3,21 +3,36 @@ package com.codenode.budgetlens
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import com.codenode.budgetlens.databinding.ActivityLoginBinding
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
 
 class LoginActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityLoginBinding
+    private lateinit var usernameField: TextInputEditText
+
+    private lateinit var passwordField: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityLoginBinding.inflate(layoutInflater)
-
         setContentView(R.layout.activity_login)
 
+        val goToHomePageActivity = Intent(this, HomePageActivity::class.java)
+
+        val loginButton: Button = findViewById(R.id.checkCredentials)
 
         val actionBar = supportActionBar
+
+        usernameField = findViewById<TextInputEditText>(R.id.usernameText)
+
+        passwordField = findViewById<TextInputEditText>(R.id.passwordText)
 
         actionBar!!.title = "Login"
 
@@ -25,14 +40,66 @@ class LoginActivity : AppCompatActivity() {
 
         val registerButton: Button = findViewById(R.id.createNewUser)
 
-        registerButton.setOnClickListener {
+        //This will redirect the user to the register page
+        registerButton.setOnClickListener(){
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
 
-//        binding.createNewUser.setOnClickListener {
-//            val intent = Intent(this, SignUpActivity::class.java)
-//            startActivity(intent)
-//        }
+        // Will eventually need to block login when user is already logged in
+        loginButton.setOnClickListener {
+
+            if(usernameField.length() == 0){
+                usernameField.error = "This field is required"
+            }
+            if(passwordField.length() == 0){
+                passwordField.error = "This field is required"
+            }
+
+                val url = "http://${BuildConfig.ADDRESS}:${BuildConfig.PORT}/loginEndpoint/"
+
+                val registrationPost = OkHttpClient()
+
+                val mediaType = "application/json".toMediaTypeOrNull()
+
+                val body = ("{\r\n" +
+                        "    \"username\": \"${usernameField.text}\",\r\n" +
+                        "    \"password\": \"${passwordField.text}\"\r\n" +
+                        "}").trimIndent().toRequestBody(mediaType)
+
+                val request = Request.Builder()
+                    .url(url)
+                    .method("POST", body)
+                    .addHeader("Content-Type", "application/json")
+                    .build()
+
+                registrationPost.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        Log.i("Response", "Got the response from server")
+                        response.use {
+                            if (response.isSuccessful) {
+                                val body = response.body?.string()
+                                if (body != null) {
+                                    Log.i("Successful", body)
+                                    startActivity(goToHomePageActivity)
+                                }
+                                else{
+                                    Log.i("Empty", "Something went wrong${response.body?.string()}")
+                                }
+
+                            } else {
+                                Log.e(
+                                    "Error",
+                                    "Something went wrong${response.body?.string()} ${response.message} ${response.headers}"
+                                )
+                            }
+                        }
+                    }
+                })
+        }
     }
 }
