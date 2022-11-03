10219194270db57da6a9bac8ed56cadbecd402d3
shahdyousefak <shahdyousefak@gmail.com>
2022-11-03 15:37:51 -0400

    ok so this is an indiv receipt oage. the figma design only showed an indiv receipt page after scanning a receipt. i just mocked it up

---
+++ app/src/main/java/com/codenode/budgetlens/receipts/MyItemRecyclerViewAdapter.kt
@@ -0,0 +1,58 @@
+package com.codenode.budgetlens.receipts
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.codenode.budgetlens.databinding.FragmentReceiptsBinding
+import com.codenode.budgetlens.receipts.placeholder.PlaceholderContent.PlaceholderItem
+
+// here im loading item 1, 2, 3 etc. to test
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MyItemRecyclerViewAdapter(
+    private val context: Context,
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentReceiptsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+        holder.parentView.setOnClickListener {
+            ReceiptInfoDialog(context).show()
+        }
+    }
+
+    //???? lmao
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentReceiptsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+        val parentView: LinearLayout = binding.parent
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
