89496d1006c2849fe3bd37a52e8737dea557cb6e
MiaMianeko <zosanivan@gmail.com>
2022-11-10 21:02:58 -0500

    create Item list activity

---
+++ app/src/main/java/com/codenode/budgetlens/items/ItemListActivity.kt
@@ -0,0 +1,12 @@
+package com.codenode.budgetlens.items
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codenode.budgetlens.R
+
+class ItemListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_list)
+    }
+}
