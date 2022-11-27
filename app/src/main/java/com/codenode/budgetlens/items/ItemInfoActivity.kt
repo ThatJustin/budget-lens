1b64698f0f68bebd5f330522194f3d7740656787
MiaMianeko <zosanivan@gmail.com>
2022-11-26 20:36:05 -0500

    the iteminfo page can now get the item Id and display the minimal data

--- app/src/main/java/com/codenode/budgetlens/items/ItemInfoActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/items/ItemInfoActivity.kt
@@ -4,23 +4,31 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.codenode.budgetlens.BuildConfig
 import com.codenode.budgetlens.R
 import com.codenode.budgetlens.common.BearerToken
 import com.codenode.budgetlens.databinding.ActivityMainBinding
 import okhttp3.*
-import org.json.JSONObject
+import org.json.JSONArray
 import java.io.IOException
 
 class ItemInfoActivity() : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var autoCompleteTextView: AutoCompleteTextView
+    private lateinit var itemPrice: TextView
+    private lateinit var itemName: TextView
+    private lateinit var itemOwner: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_item_info)
+        //get the item Id from the previous page
 
+        val itemId: String? = intent.getStringExtra("itemId")
+        Log.i("this is item info page ", itemId.toString())
         //create category arrays
         //here is where you get the array from the database
         val categories = resources.getStringArray(R.array.category_place_holder)
@@ -30,8 +38,11 @@ class ItemInfoActivity() : AppCompatActivity() {
         autoCompleteTextView = findViewById(R.id.category_dropdown)
         autoCompleteTextView.setAdapter(arrayAdapter)
 
-        //get the item Id from the previous page
-        val itemId = intent.getStringExtra("itemId")
+        //setup item display fields
+        itemPrice = findViewById(R.id.item_info_price)
+        itemName = findViewById(R.id.item_info_name)
+        itemOwner = findViewById(R.id.item_original_owner)
+
 
         //get the item data
         val url = "http://${BuildConfig.ADDRESS}:${BuildConfig.PORT}/items/${itemId}/"
@@ -47,22 +58,28 @@ class ItemInfoActivity() : AppCompatActivity() {
                 e.printStackTrace()
             }
 
-            override fun onResponse(call: Call, response: Response) {
-                response.use{
-                    if(response.isSuccessful){
-                        val responseBody=response.body?.string()
-                        if(responseBody!=null){
-                            val price = JSONObject(responseBody.toString()).getString("price")
-                            val name = JSONObject(responseBody.toString()).getString("name")
-                            val user =  JSONObject(responseBody.toString()).getString("user")
 
-                        }else {
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        if (responseBody != null) {
+                            val itemArray = JSONArray(responseBody.toString())
+                            val item = itemArray.getJSONObject(0)
+                            val price = item.getString("price")
+                            val name = item.getString("name")
+                            val user = item.getString("user")
+                            itemPrice.text = price
+                            itemName.text = name
+                            itemOwner.text = user
+
+                        } else {
                             Log.i(
                                 "Error",
                                 "Something went wrong ${response.message} ${response.headers}"
                             )
                         }
-                    }else {
+                    } else {
                         Log.e(
                             "Error",
                             "Something went wrong ${response.message} ${response.headers}"
@@ -71,5 +88,7 @@ class ItemInfoActivity() : AppCompatActivity() {
                 }
             }
         })
+
+
     }
 }
