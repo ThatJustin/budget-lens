a4f42f5d4b2ee7a78e94bf8a2c5121fd9798b32c
Yi Heng Yan <73037987+MiaMianeko@users.noreply.github.com>
2022-11-26 19:09:15 -0500

    [Feacture]:Bud-164 item list page (#26)
    * create Item list activity
    
    * add items page and edit item models
    
    * items page connected
    
    * uncomment some not important thing
    
    * fix the bug
    
    * delete unused commented out code
    
    * add navbar
    
    * fix(items): hide progress bar after loading is done
    
    * add the month (need to be updated with actual words), total
    
    * add switch statement to display month (could be improved in the future), adjust font size
    
    Co-authored-by: ThatJustin <62494960+ThatJustin@users.noreply.github.com>

---
+++ app/src/main/java/com/codenode/budgetlens/items/ItemListActivity.kt
@@ -0,0 +1,86 @@
+package com.codenode.budgetlens.items
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.codenode.budgetlens.R
+import com.codenode.budgetlens.common.ActivityName
+import com.codenode.budgetlens.common.CommonComponents
+import com.codenode.budgetlens.data.Items
+import com.codenode.budgetlens.data.UserItems.Companion.loadItemsFromAPI
+import com.codenode.budgetlens.data.UserItems.Companion.pageNumber
+import com.codenode.budgetlens.data.UserItems.Companion.userItems
+
+class ItemListActivity : AppCompatActivity() {
+    private lateinit var itemList: MutableList<Items>
+    private var itemsListRecyclerView: RecyclerView? = null
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var itemAdapter: RecyclerView.Adapter<ItemsRecyclerViewAdapter.ViewHolder>
+    private var pageSize = 5
+    private lateinit var itemTotal: TextView
+    private lateinit var result: Pair<MutableList<Items>,Double>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_list)
+        //search/sort/filter variable
+        var additionalData = ""
+        val searchBar: SearchView = findViewById(R.id.search_bar_text)
+        CommonComponents.handleTopAppBar(this.window.decorView, this, layoutInflater)
+        CommonComponents.handleNavigationBar(ActivityName.ITEMS, this, this.window.decorView)
+        itemTotal = findViewById(R.id.item_cost_value)
+
+        userItems.clear()
+
+        pageNumber = 1
+
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+
+        //load the list
+
+        result = loadItemsFromAPI(this, pageSize, additionalData)
+        itemList = result.first
+        itemTotal.text = result.second.toString()
+
+        val context = this
+        itemsListRecyclerView = findViewById(R.id.item_list)
+        progressBar.visibility = View.VISIBLE
+
+        if (itemList.isEmpty()) {
+            itemsListRecyclerView!!.visibility = View.GONE
+            progressBar.visibility = View.GONE
+        }
+
+        if (itemsListRecyclerView != null) {
+            itemsListRecyclerView!!.setHasFixedSize(true)
+            linearLayoutManager = LinearLayoutManager(this)
+            itemsListRecyclerView!!.layoutManager = linearLayoutManager
+            itemAdapter = ItemsRecyclerViewAdapter(itemList)
+
+            itemsListRecyclerView!!.adapter = itemAdapter
+            progressBar.visibility = View.GONE
+            itemsListRecyclerView!!.addOnScrollListener(object :
+                RecyclerView.OnScrollListener() {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    progressBar.visibility = View.VISIBLE
+                    if (!recyclerView.canScrollVertically(RecyclerView.FOCUS_DOWN) && recyclerView.scrollState == RecyclerView.SCROLL_STATE_IDLE) {
+                        result = loadItemsFromAPI(context, pageSize, additionalData)
+                        itemList = result.first
+                        itemTotal.text = result.second.toString()
+                        itemAdapter.notifyDataSetChanged()
+                    }
+                    progressBar.visibility = View.GONE
+                }
+            }
+            )
+        }
+    }
+}
