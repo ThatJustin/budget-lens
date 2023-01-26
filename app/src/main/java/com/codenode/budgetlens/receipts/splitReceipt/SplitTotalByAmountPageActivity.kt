96ed4437c2c9251642cdb96cdc63701e0d8506a4
shirlywy <shirly970711@gmail.com>
2023-01-26 18:17:14 -0500

    activity set up for split total by amount page

--- app/src/main/java/com/codenode/budgetlens/receipts/splitReceipt/SplitTotalByAmountPageActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/receipts/splitReceipt/SplitTotalByAmountPageActivity.kt
@@ -1,4 +1,18 @@
 package com.codenode.budgetlens.receipts.splitReceipt
 
-class SplitTotalByAmountPageActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.codenode.budgetlens.R
+import com.google.android.material.button.MaterialButtonToggleGroup
+
+class SplitTotalByAmountPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_split_total_by_amount)
+        val toggleButton: MaterialButtonToggleGroup = findViewById(R.id.splitTotalToggleButton)
+
+
+    }
 }
