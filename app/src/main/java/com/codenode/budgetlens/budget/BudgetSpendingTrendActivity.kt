b428b8933f384c3fc3347a58129b590a9d511b1c
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2023-02-03 21:05:01 -0500

    fix: spending trend bottom navbar works now

--- app/src/main/java/com/codenode/budgetlens/budget/BudgetSpendingTrendActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/budget/BudgetSpendingTrendActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codenode.budgetlens.R
+import com.codenode.budgetlens.common.ActivityName
+import com.codenode.budgetlens.common.CommonComponents
 import com.codenode.budgetlens.data.Trend
 
 class BudgetSpendingTrendActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_budget_spending_trend)
-        var layoutManager =
+        CommonComponents.handleTopAppBar(this.window.decorView, this, layoutInflater)
+        CommonComponents.handleNavigationBar(ActivityName.ITEMS, this, this.window.decorView)
+
+        val layoutManager =
             LinearLayoutManager(baseContext, LinearLayoutManager.VERTICAL, false)
 
-        var trendList = findViewById<RecyclerView>(R.id.trendlist)
+        val trendList = findViewById<RecyclerView>(R.id.trendlist)
         trendList.layoutManager = layoutManager
 
         val trendlist = arrayListOf<Trend>()
