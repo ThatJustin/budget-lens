4835ad043665229bec9fa4e6ecd1c880f5b3ef43
Mateo <mateo.palomino21@hotmail.com>
2023-02-16 23:39:43 -0500

    fix/bud-279: added todo for api update

--- app/src/main/java/com/codenode/budgetlens/common/ScanningReceiptActivity.kt
+++ app/src/main/java/com/codenode/budgetlens/common/ScanningReceiptActivity.kt
@@ -135,12 +135,11 @@ class ScanningReceiptActivity : AppCompatActivity() {
 
     private fun confirm(context: Context, goToHomePageActivity: Intent) {
 
+        //TODO: UPDATE THE API FOR SAVING PICTURE AS A RECEIPT AS SOON AS AMIR PUSHES PR
+
         val url = "http://${BuildConfig.ADDRESS}:${BuildConfig.PORT}/api/receipts/"
-
         val registrationPost = OkHttpClient()
-
         val mediaType = "text/plain".toMediaTypeOrNull()
-
         val requestBody = MultipartBody.Builder()
             .setType(MultipartBody.FORM)
             .addFormDataPart("receipt_image", tempFile,
@@ -154,7 +153,6 @@ class ScanningReceiptActivity : AppCompatActivity() {
             .addFormDataPart("currency","USD")
             .addFormDataPart("important_dates","1999-01-01")
             .build()
-
         val request = Request.Builder()
             .url(url)
             .method("POST", requestBody)
