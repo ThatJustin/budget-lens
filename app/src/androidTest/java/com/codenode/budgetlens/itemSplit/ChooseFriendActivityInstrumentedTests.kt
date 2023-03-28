d1069631c962261ad1d6c4fcb18651e9539bab9b
Mohamed <67757119+mamineahmadi@users.noreply.github.com>
2023-03-27 22:09:44 -0400

    refactoring tests

--- app/src/androidTest/java/com/codenode/budgetlens/itemSplit/ChooseFriendActivityInstrumentedTests.kt
+++ app/src/androidTest/java/com/codenode/budgetlens/itemSplit/ChooseFriendActivityInstrumentedTests.kt
@@ -1,42 +1,30 @@
 package com.codenode.budgetlens.itemSplit
 
 import android.content.Intent
-import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import com.codenode.budgetlens.R
-import com.codenode.budgetlens.friends.FriendsPageActivityInstrumentedTests
-import com.codenode.budgetlens.home.HomePageActivity
-import com.codenode.budgetlens.login.LoginActivity
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.espresso.action.ViewActions.swipeDown
-import androidx.test.espresso.action.ViewActions.swipeUp
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import com.codenode.budgetlens.receipts.ReceiptsListPageActivity
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ChooseFriendActivityInstrumentedTests {
-
+    // This is used to clear the shared preferences before each test
     companion object {
         @BeforeClass
         fun clearStorage() {
-            InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("pm clear PACKAGE NAME")
-                .close()
+            InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("pm clear PACKAGE NAME").close()
         }
     }
 
@@ -45,52 +33,18 @@ class ChooseFriendActivityInstrumentedTests {
 
     @Before
     fun setup() {
-        FriendsPageActivityInstrumentedTests.clearStorage()
-        var intent = Intent(
-            InstrumentationRegistry.getInstrumentation().targetContext,
-            LoginActivity::class.java
-        )
+        clearStorage()
+        var intent = Intent(InstrumentationRegistry.getInstrumentation().targetContext, ChooseFriendActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        androidx.core.content.ContextCompat.startActivity(
-            InstrumentationRegistry.getInstrumentation().targetContext,
-            intent,
-            null
-        )
-
-        // The following inputs the username and password into the login page and clicks the login button after making
-        // sure to close the keyboard
-        onView(withId(R.id.usernameText)).perform(
-            ViewActions.typeText("Test1234"),
-            ViewActions.closeSoftKeyboard()
-        ).check(matches(withText("Test1234")))
-        onView(withId(R.id.passwordText)).perform(
-            ViewActions.typeText("test1234"),
-            ViewActions.closeSoftKeyboard()
-        ).check(matches(withText("test1234")))
-        onView(withId(R.id.checkCredentials)).perform(click())
-            .check(matches(ViewMatchers.isDisplayed()))
-        intent = Intent(
-            InstrumentationRegistry.getInstrumentation().targetContext,
-            ChooseFriendActivity::class.java
-        )
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        androidx.core.content.ContextCompat.startActivity(
-            InstrumentationRegistry.getInstrumentation().targetContext,
-            intent,
-            null
-        )
-
+        startActivity(InstrumentationRegistry.getInstrumentation().targetContext, intent, null)
     }
 
     @Test
-    fun test_split_item_list_activity_is_displayed_and_scrollable() {
-        // This test checks to see if the split item list activity is displayed and scrollable
-        // by first checking if it is displayed when the SplitItemListActivity is started
-        // and then scrolling down and up the list to make sure that it is scrollable
-        onView(withId(R.id.friends_list)).perform(swipeUp())
-        onView(withId(R.id.friends_list)).perform(swipeDown())
-        onView(withId(R.id.friends_list)).perform(click())
-
+    fun test_split_item_list_activity_is_displayed() {
+        // This test checks to see if the split item list activity is displayed by
+        // checking if it is displayed when the SplitItemListActivity is started
+        onView(withId(R.id.cancel_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.confirm_button)).check(matches(isDisplayed()))
     }
 
     @Test
@@ -103,7 +57,9 @@ class ChooseFriendActivityInstrumentedTests {
     @Test
     fun test_split_item_list_activity_cancel_button() {
         onView(withId(R.id.cancel_button)).perform(click())
-        // Add an assertion or check to verify that the expected behavior occurs when the cancel button is clicked.
+        intent = Intent(InstrumentationRegistry.getInstrumentation().targetContext, ChooseFriendActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(InstrumentationRegistry.getInstrumentation().targetContext, intent, null)
     }
 
     @Test
